6f14284156e4805c4567fc7c36954a850d389d41
Paras Teli <parasteli2004@gmail.com>
2025-04-15 20:52:12 +0530

    bugs

--- pages/login.js
+++ pages/login.js
@@ -76,7 +76,7 @@ export default function Login() {
           </button>
         </form>
         <p className="text-sm text-center text-gray-600">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <Link href="/signup" className="text-blue-500 hover:underline">
             Sign up
           </Link>
